eb1501e0f7511dff8884d3753005d5da214be2fb
AlahiraJ <alahirajeffrey@gmail.com>
2021-12-18 10:31:52 +0100

    completed sleep chekcer script

---
+++ sleep_checker.py
@@ -0,0 +1,26 @@
+from plyer import notification
+import time
+import subprocess
+
+NOTIFICATION_COUNTER = 0
+
+while True:
+    # sleep for 20 minutes before notification
+    time.sleep(10)
+
+    # show notification
+    notification.notify(
+        title='Sleep checker',
+        message='Are you awake or asleep',
+        app_name="",
+        app_icon=r"C:\Users\Jeffrey Alahira\Documents\Python\sleep_checker\sleep.ico",
+        timeout=2,
+    )
+
+    # count number of times notification shows
+    NOTIFICATION_COUNTER += 1
+
+    # hibernate system if notification shows three times
+    if(NOTIFICATION_COUNTER) == 3:
+        subprocess.run(['shutdown', '-h'])
+        exit()
